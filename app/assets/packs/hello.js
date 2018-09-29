74daff3b4d9c84ea74316feaf65cb873fe7be8b6
Ramilia Nigmatullina <RamiliaNigmatullina@gmail.com>
2018-09-30 01:11:47 +0300

    add hello-react component

---
+++ app/assets/packs/hello.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Hello = props => (
+  <div className='react-app-wrapper'>
+    <h5 className='hello'>
+      {props.message} {props.name}!
+    </h5>
+  </div>
+)
+
+// Render component with data
+document.addEventListener('DOMContentLoaded', () => {
+  const node = document.getElementById('hello')
+  const data = JSON.parse(node.getAttribute('data'))
+
+  ReactDOM.render(<Hello {...data} />, node)
+})
